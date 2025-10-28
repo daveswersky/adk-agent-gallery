246763bf4bc898a2b7fa570e8969a1d504379a59
dswersky <dswersky@gmail.com>
2025-10-28 12:16:45 -0400

    refactor(frontend): Update pin icon

--- frontend/components/icons.tsx
+++ frontend/components/icons.tsx
@@ -15,7 +15,7 @@ export const StopIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
 
 export const PinIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 3.75V16.5L12 21l-4.5-4.5V3.75M16.5 3.75h-9" />
   </svg>
 );
 
